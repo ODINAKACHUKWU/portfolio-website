e372a6371bbc75597b1a75ebd8dc8dcfcfb5176b
Odinakachukwu Solomon Ezeobika <solomonzbk@gmail.com>
2025-09-02 17:58:50 -0700

    Update favicon

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-// import { FaGoogle, FaFacebookF, FaSpotify, FaShopify } from "react-icons/fa";
-import { HStack } from '@chakra-ui/react';
+import { HStack, Icon } from '@chakra-ui/react';
 // import { Icon } from "@chakra-ui/icon";
+import { Sun, Moon, Instagram, Github, Linkedin } from 'lucide-react';
 
 interface Props {}
 
 export const Social: React.FC<Props> = () => {
   return (
     <HStack>
-      {/* <Icon as={FaFacebookF} boxSize="50" />
-			<Icon as={FaGoogle} boxSize="50" />
-			<Icon as={FaSpotify} boxSize="50" />
-			<Icon as={FaShopify} boxSize="50" /> */}
+      {/* <Icon as={FaFacebookF} boxSize="50" /> */}
+      {/* <Icon as={FaGoogle} boxSize="50" /> */}
+      {/* <Icon as={FaSpotify} boxSize="50" /> */}
+      {/* <Icon as={FaShopify} boxSize="50" /> */}
+      {/* <FaFacebook size={24} /> */}
+      {/* </Icon> */}
+      {/* <Icon size="lg" color="tomato"> */}
+      {/* <Md3dRotation /> */}
+      {/* </Icon> */}
     </HStack>
   );
 };
